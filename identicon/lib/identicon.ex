42089637310fe6575c40d3347076569e25cb2b3d
crzepka <crzepka@cars.com>
2019-01-04 16:44:54 -0600

    intial commit for identicon

---
+++ identicon/lib/identicon.ex
@@ -0,0 +1,32 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicon.hello()
+      :world
+
+  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+
+    [r, g, b]
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+      %Identicon.Image{hex: hex}
+  end
+end
